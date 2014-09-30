f375c359be418cff41f23716a835768b8cb87a51
Tim Sodhi <timsodhi@timsodhi.vpuad.buffalo.edu>
2014-09-30 10:49:40 -0400

    Initial Commit

---
+++ PostcardApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PostcardApp
+//
+//  Created by Tim Sodhi on 9/30/14.
+//  Copyright (c) 2014 Tim Sodhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
