124a61d125a4ddd19edb3057d145d5ba54ddd4cc
timothevs <tim@edvpro.com>
2014-10-01 11:15:02 -0400

    Changed to add another label
    It was fun

--- PostcardApp/ViewController.swift
+++ PostcardApp/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var enterNameTextField:UITextField!
     @IBOutlet weak var enterMessageTextField:UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -25,11 +26,14 @@ class ViewController: UIViewController {
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         messageLabel.hidden=false;
+        nameLabel.hidden=false;
         messageLabel.text=enterMessageTextField.text;
         messageLabel.textColor=UIColor.purpleColor();
         enterMessageTextField.text="";
         enterMessageTextField.resignFirstResponder();
-        
+        nameLabel.text=enterNameTextField.text;
+        nameLabel.textColor=UIColor.blueColor();
+        enterNameTextField.text="";
         
     }
 
